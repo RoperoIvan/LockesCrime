ea18cea0cdbe91f8077bb3137e252db42bf98dcf
RoperoIvan <ivan_ropero98@hotmail.com>
2021-04-28 10:26:26 +0200

    fixed camera bounds

--- LockesCrime/Assets/Scripts/MouseCamLook.cs
+++ LockesCrime/Assets/Scripts/MouseCamLook.cs
@@ -12,41 +12,25 @@ using UnityEngine;
 
 public class MouseCamLook : MonoBehaviour
 {
+    public float speedV = 2.0f;
+    public float speedH = 2.0f;
+    public float angleClamp = 0f;
 
-    [SerializeField]
-    public float sensitivity = 5.0f;
-    [SerializeField]
-    public float smoothing = 2.0f;
-    // the chacter is the capsule
-    public GameObject character;
-    // get the incremental value of mouse moving
-    private Vector2 mouseLook;
-    // smooth the mouse moving
-    private Vector2 smoothV;
-
-    // Use this for initialization
-    void Start()
-    {
-        character = this.transform.parent.gameObject;
-    }
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
         if(!DialogueManager.hasDialog)
         {
-            // md is mosue delta
-            var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-            md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
-            // the interpolated float result between the two float values
-            smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
-            smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
-            // incrementally add to the camera look
-            mouseLook += smoothV;
+            rotationY += speedV * Input.GetAxis("Mouse X");
+            rotationX -= speedH * Input.GetAxis("Mouse Y");
 
-            // vector3.right means the x-axis
-            transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
-            character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+            rotationX = Mathf.Clamp(rotationX, -angleClamp, angleClamp);
+
+            transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
+
         }
     }
 }
