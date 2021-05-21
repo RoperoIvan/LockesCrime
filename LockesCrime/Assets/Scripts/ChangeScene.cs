30407e74b7757cdd026297971c856cf4a9d389f9
didaclis <36189205+didaclis@users.noreply.github.com>
2021-05-21 19:15:43 +0200

    lockes crime

---
+++ LockesCrime/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeScene()
+    {
+        if(SceneManager.GetActiveScene().name == "Menu")
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
