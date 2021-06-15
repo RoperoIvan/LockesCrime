abf7e150b99e792102626dffc5278e542b230b9a
didaclis <36189205+didaclis@users.noreply.github.com>
2021-06-15 17:09:41 +0200

    game loop

---
+++ LockesCrime/Assets/goMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goMenu : MonoBehaviour
+{
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void returnToLobby()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
